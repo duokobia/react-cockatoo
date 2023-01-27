ed6668ae2a247f5416e80df46f57752ccbe4e4c7
Daniel Okobia <duokobia@gmail.com>
2023-01-27 12:40:00 +0100

    Fix props passing issue

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -3,27 +3,38 @@ import React, {useState} from "react";
 
 let AddTodoForm = ({onAddTodo}) => {
 
-  const[inputText, setInputText] = useState("");
+  // const[inputText, setInputText] = useState("");
   
 
   let handleAddTodo = function (event){
-    event.preventDefault(); // Prevent the default behaviour of the form submit.
-
-    // Retrieves the value of the title element from 
-    // the evenet target and store it in a variable named todoTitle.
-    let todoTitle = inputText; 
+    event.preventDefault(); // Prevents the default behaviour of the form submit.
+   
+    // Retrieves the value of the title passed into the input tag by assessing it using the "name" attribute renamed as "title"
+    // (name="title"), and storing it inside the variable "todoTitle".
+    // By using "let todoTitle = event.target.title.value;", the app didn't work because the variable is a constant value.
+    const todoTitle = event.target.title.value; 
     console.log(todoTitle);
-    setInputText(event.target.value);
-    setInputText(""); // Reset form onSubmit.
-    onAddTodo(todoTitle); // Invokes onAddTodo callback prop and pass the todoTitle as an argument inside this handleAddTodo function
-    
+    event.target.reset();// Reset form onSubmit.
+
+    // The setter function "setNewTodo" has being passed as a prop under the name "onAddTodo" to the AddTodoForm.js 
+    // component inside the "App.js" component. The "onAddTodo", which is a callback function inside this "handleAddTodo"
+    // function, accepts the variable "todoTitle" as an argument here inside the AddTodoForm.js component(From the form below).
+    // Once invoked, the "onAddTodo" function grabs the "todoTitle" variable from the form, then renders the value stored inside
+    // "todoTitle" in the AddTodoForm.js component nested inside the App.js using the setter function "setNewTodo". This is
+    // because the "onAddTodo" function was set as this setter function in the App.js while being passed as a prop to the
+    // AddTodoForm.js component. So in this case, the real functionality passed down from the App.js component(parent), to the 
+    // AddTodoForm.js component(child), is the setter function "setNewTodo".
+    onAddTodo(todoTitle);
+
+   
+
   };
 
 
   return (
     <form onSubmit={(event) => handleAddTodo(event)}>
         <label htmlFor="todoTitle">Title</label>
-        <input id="todoTitle" name="title" value = {inputText} onChange={(event) => setInputText(event.target.value)}></input>
+        <input id="todoTitle" name="title"></input>
         <button type="submit">Add</button>
     </form>
   );
--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -5,7 +5,11 @@ const TodoListItem = (props) => {
   return ( 
     <div>
       {/* Updating todo object reference to come from props is done 
-      by adding "props." to "todo.title" */}
+      by adding "props." to "todo.title".
+
+      "props.todo.title" means the "props" passed into this function(component) has an object in it called "todo".
+      So get me the value of an item in this "todo" object using the key: "title".
+      */}
         <li>{props.todo.title}</li>
     </div>
   );
