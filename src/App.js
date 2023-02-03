d55b38a3789b7a2db277d1516c08b7e757f2162a
Daniel Okobia <duokobia@gmail.com>
2023-02-03 15:02:56 +0100

    Submission for Week5

--- src/App.js
+++ src/App.js
@@ -4,16 +4,46 @@ import AddTodoForm from "./AddTodoForm";
 
 function App() {
 
-  const [newTodo, setNewTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  // The spread operator and rest operator both have the same syntax(...).
+
+  // JS spread operator allows you to literally spread all key/value pairs
+  // of an object to another object. It happens on the right hand side of an
+  // asssignment.
+
+  // ### 1 ###
+  // JS rest operator happens always as the last part of an object destructuring.
+  // That is, on the left hand side of an asssignment. It is always used to 
+  // separate an object, in this case, todoList, from some of its own properties,
+  // or other properties, in this case, newTodo. In the code below, only the todoList 
+  // item, i.e (rest or remaining item) gets the effect of the spread operator when 
+  // setTodoList is invoked. Hence, this is a rest operator and not spread operator. 
+
+  // ### 2 ###
+  // AddTodoForm.js is a controlled component. This is because its state is managed
+  // by its parent component(App.js). The parent component(App.js) provides the values
+  // for the controlled component(AddTodoForm.js) through props(onAddTodo), and the 
+  // controlled component(AddTodoForm.js) then triggers updates in the parent component
+  // (App.js) through callback function(addTodo).
+
+
+  let addTodo = function(newTodo){
+    setTodoList([newTodo, ...todoList]); /* 1 */ 
+  };
 
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Todo List</h1>
-      <AddTodoForm onAddTodo = {setNewTodo}/>
-      <p>{newTodo}</p>
-      <TodoList />
+
+      { /* 2 */ }
+      <AddTodoForm onAddTodo = {addTodo}/> 
+      <p>{}</p>
+      <TodoList todoList = {todoList} />
     </div>
   );
 };
 
 export default App;
+
+
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,72 +2,13 @@ import React from "react";
 import TodoListItem from "./TodoListItem";
 
 
-const todoList = [
-    {
-      id: 1,
-      title: "Complete ReactJs assignment",
-    },
-    {
-      id: 2,
-      title: "Complete NodeJs assignment",
-    },
-    {
-      id: 3,
-      title: "Read documentations for topics covered for the week for better understanding of each concept",
-    },
-    {
-      id: 4,
-      title: "Do personal coding assignments",
-    },
-    {
-      id: 5,
-      title: "Research difficult to grasp topics learnt and write personal notes as comments on each difficult to understand code block",
-    },
-    {
-      id: 6,
-      title: "Slack message my mentors and colleagues and book for sessions ahead",
-    },
-    {
-      id: 7,
-      title: "Plan for next week CTD classes and sessions",
-    },
-    {
-      id: 8,
-      title: "Check and reply e-mails",
-    },
-    {
-      id: 9,
-      title: "Continue my job search as a junior Frontend devloper",
-    },
-    {
-      id: 10,
-      title: "Work on interviewing tips",
-    },
-];
-
-
-const TodoList = () => {
+const TodoList = ({todoList}) => {
   return (
-        <ul>
-          {/* Each of the elements(objects) in the array("todoList") above is referred to as "todo" or whatever name I call it.
-          I also tried using "list" and it worked just fine. The repetition of the name todo = {todo} confused me at first.
-          So assign each of these elements("todo"s) inside the array("todoList") to the "todo" attribute.I also tried using "list",
-          that is, instead of using (todo = {todo}), use (list = {todo}) to help you identify each parameter. 
-          The "todoList" variable is passed to the "ToDoListItem" component under the name "todo" using the "todo" attribute (todo = {todo})
-          or if you used "list" instead. So once the "ToDoListItem" component is instantiated, it assigns "todo" to the(its) "todo" HTML attribute
-          or "list" attribute if you tried using (list = {todo}).
-  
-          After passing it to the "ToDoListItem" component, we can access it as "todo" property(or "list" if you tried using (list = {todo})) 
-          from the props object in the "ToDoListItem" component's function signature using {props.todo.title} or {props.list.title}.
-
-          JS map() method creates a new array from calling a function for every array element: 
-          For every element("todo") in the array("todoList"), the todoList.map() function uses their unique identifiers("id"s)
-          to get their "title"s and produce a list(<li>{props.todo.title}</li>) of "title"s that will be displayed in the "ToDoListItem" 
-          component once instantiated. */}
-          {todoList.map(todo => 
-            <TodoListItem key = {todo.id} todo = {todo} /> 
-          )}
-        </ul>
+    <ul>
+      {todoList.map(todo => 
+        <TodoListItem key = {todo.id} todo = {todo} /> 
+      )}
+    </ul>
   );
 };
 
